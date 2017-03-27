470964fc5df828df3fbc007814edfe9f63550416
billhu1996 <billhu1996@gmail.com>
2017-03-27 15:57:18 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -4,5 +4,5 @@
 username=你的用户名
 password=你的密码
 
-data="buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network&username="$username"&password="$password
-curl -d $data "https://1.1.1.1/login.html"
+data="DDDDD="$username"&upass="$password
+curl -d $data "http://211.82.99.97:801/eportal/?c=ACSetting&a=Login&mac=000000000000"
