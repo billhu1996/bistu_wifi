dc04f1e0f8dc4e11cf56c8a49a9e39f826090308
billhu1996 <billhu1996@gmail.com>
2017-03-27 15:58:49 +0800

    使用curl的绝对地址以修复初次登录系统或者没有激活环境变量时候脚本运行错误 by @arcsecw

--- login.sh
+++ login.sh
@@ -5,4 +5,4 @@ username=你的用户名
 password=你的密码
 
 data="DDDDD="$username"&upass="$password
-curl -d $data "http://211.82.99.97:801/eportal/?c=ACSetting&a=Login&mac=000000000000"
+/usr/bin/curl -d $data "http://211.82.99.97:801/eportal/?c=ACSetting&a=Login&mac=000000000000"
